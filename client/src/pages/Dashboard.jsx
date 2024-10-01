29c344839e7bfd2f39820e7fca156eec621f255c
yahayong <luthfansya3504@gmail.com>
2024-10-01 10:32:46 +0700

    haya

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { UserContext } from "../../context/userContext";
 
+
 const Dashboard = () => {
   const { user } = useContext(UserContext);
 
@@ -9,7 +10,11 @@ const Dashboard = () => {
     return <Navigate to="/" />;
   }
 
-  return <div>{!!user && <h1>Hi, {user.name}!</h1>}</div>;
+  return (
+    <div style={{ position: "absolute", top: 18, right: 120 }}>
+      {!!user && <h1>Hi, {user.name}!</h1>}
+    </div>
+  );
 };
 
 export default Dashboard;
