fb41feaf03290bdbc5382da2db80857b7e237aef
hawyeet <53439203+hawyeet@users.noreply.github.com>
2024-10-01 09:13:30 +0700

    Revert "Haya"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,12 @@
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
-import Signup from "./pages/Signup";
+import Register from "./pages/Register";
 import Login from "./pages/Login";
 import axios from "axios";
 import { Toaster } from "react-hot-toast";
 import { Routes, Route } from "react-router-dom";
 import { UserContextProvider } from "../context/userContext";
-import "./App.css";
 
 axios.defaults.baseURL = "http://localhost:8000";
 axios.defaults.withCredentials = true;
@@ -20,13 +19,11 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </UserContextProvider>
   );
 };
-
-
 
 export default App;
