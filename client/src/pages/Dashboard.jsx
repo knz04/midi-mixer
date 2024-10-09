24d9bf59ad3ba2066a500d64e92880316e313337
hawyeet <53439203+hawyeet@users.noreply.github.com>
2024-10-09 09:23:58 +0700

    Added middleware

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { UserContext } from "../../context/userContext";
-
+import PresetList from "../components/PresetList";
 
 const Dashboard = () => {
   const { user } = useContext(UserContext);
@@ -11,11 +11,11 @@ const Dashboard = () => {
   }
 
   return (
-
-    <div style={{height: "100vh", width: "100vw "}}>
+    <div style={{ height: "100vh", width: "100vw " }}>
       <div style={{ position: "absolute", top: 20, right: 140 }}>
-      {!!user && <h1>Hi, {user.name}!</h1>}
-    </div>
+        {!!user && <h1>Hi, {user.name}!</h1>}
+      </div>
+      <PresetList />
     </div>
   );
 };
