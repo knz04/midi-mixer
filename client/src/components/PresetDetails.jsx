52339527bd02d32183af11e9d4393ba900a03ec5
yahayong <luthfansya3504@gmail.com>
2024-11-02 20:44:02 +0700

    ganti warna button doang

--- client/src/components/PresetDetails.jsx
+++ client/src/components/PresetDetails.jsx
@@ -47,7 +47,7 @@ export default function PresetDetails({
         </button>
         <button
           onClick={handleOpenForm}
-          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
+          className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-700"
         >
           Create a New Preset
         </button>
