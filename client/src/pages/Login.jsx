2e804fbd784ca3b1e38861ecd557c98378f1ccc8
hawyeet <53439203+hawyeet@users.noreply.github.com>
2024-12-25 17:57:57 +0700

    Revert "Minor padding changes"
    This reverts commit 80760d7e1fce70982a432b86592bd4674a97145d.

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -46,9 +46,9 @@ export default function Login() {
         <img src={COVER_IMAGE} className="w-full h-full object-cover" />
       </div>
 
-      <div className="w-1/2 h-full bg-[#FFFFFF] flex flex-col p-28 justify-between">
+      <div className="w-1/2 h-full bg-[#FFFFFF] flex flex-col p-20 justify-between">
         <form onSubmit={loginUser}>
-          <div className="w-full my-[60px] flex flex-col max-w-[500px]">
+          <div className="w-full my-[100px] flex flex-col max-w-[500px]">
             <div className="w-full flex flex-col mb-[100px]">
               <h3 className="text-2x1 font-semibold mb-2">Login</h3>
               <p className="text-base mb-2">Welcome back!</p>
